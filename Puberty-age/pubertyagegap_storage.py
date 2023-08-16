bd295e41be0f46f952879f59ddc39dc6897dcc0a
clarefmccann <clare.mccann12@gmail.com>
2023-08-15 18:16:56 -0700

    updating pipeline to be longitudinal

--- Puberty-age/pubertyagegap_storage.py
+++ Puberty-age/pubertyagegap_storage.py
@@ -1,28 +1,20 @@
 import pandas as pd
-
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 from sklearn.preprocessing import StandardScaler
-
 from sklearn.model_selection import GroupKFold
-
 from sklearn.linear_model import LinearRegression
-
 import joblib
-
 import seaborn as sns
-
 import matplotlib.colors as mcolors
-
 
 # Make a list from sex
 sexes = ["female", "male"]
 
 # Make a dictionary from hormones for each sex
-hormones = {"female": ["tst", "dhea"],
-            "male": ["tst","dhea"]}
+hormones = {"female": ["tst", "dhea"], "male": ["tst","dhea"]}
+
+subsets = {"1", "2", "3", "4"}
 
 # Make a dictonary from each sub items of PDS for each sex
 pds = {
@@ -30,7 +22,7 @@ pds = {
     "male": ['growth_spurt', "body_hair", 'voice_deep', 'face_hair', 'skin_change',]
 }
 
-# Define a function to remove regression to the mean
+# Define a function to remove regression to the mean effect
 def remove_regression_to_mean_effect(y, y_pred):
     reg = LinearRegression().fit(np.array([y]).T, np.array([y_pred - y]).T)
     rtm_a = reg.coef_[0]
@@ -38,124 +30,119 @@ def remove_regression_to_mean_effect(y, y_pred):
     y_pred_rtm = (y_pred - rtm_b - rtm_a * y)
     return y_pred_rtm
 
-# implemented the fitted model on whole sample with cross validation to calculate the puberty age gap
+# Iterate over sex and subsets
 for sex in sexes:
-    filename = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/typical_{}.csv'.format(sex)
-    dataname = 'typical_{}'.format(sex)
-    predictors = {
-        "hormone_age": hormones[sex],
-        "pds_age": pds[sex],
-        "pubertyage": pds[sex] + hormones[sex],
+    for subset in subsets:
+        filename = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/typical_{}_{}.csv'.format(sex, subset)
+        dataname = 'typical_{}_{}'.format(sex, subset)
+        predictors = {
+            "hormone_age": hormones[sex],
+            "pds_age": pds[sex],
+            "pubertyage": pds[sex] + hormones[sex]
+        }
+        
+        to_predict_puberty = 'age'
+        
+        for predictor in predictors:
+            print('#' * 80)
+            print('# Running: sex=[{}], subset=[{}], predictor=[{}]'.format(sex, subset, predictor))
+            print('#' * 80)
+            
+            additional_dataset_test = None
 
-    }
-    to_predict_puberty = 'age'
-    for predictor in predictors:
-        print('#'*80)
-        print('# Running: sex=[{}], predictor=[{}]'.format(sex, predictor))
-        print('#'*80)
-        additional_dataset_test = None
+            group_by = 'family_id'
+            subject_col = 'id'
 
-        group_by = 'family_id'
-        subject_col = 'id'
+            # group split options
+            n_splits = 1
+            train_size = .9
+            random_state = 0
 
-        # group split options
-        n_splits = 1
-        train_size = .9
-        random_state = 0
+            all_features_puberty = predictors[predictor] + [to_predict_puberty]
 
-        all_features_puberty = predictors[predictor] + [to_predict_puberty]
+            mydfpuberty = pd.read_csv(filename)
 
-        mydfpuberty = pd.read_csv(filename)
+            # remove nan
+            nanmask = mydfpuberty[all_features_puberty].isna().any(axis=1)
+            mycleandf_puberty = mydfpuberty[~nanmask].copy()
 
-        # remove nan
-        nanmask = mydfpuberty[all_features_puberty].isna().any(axis=1)
-        mycleandf_puberty = mydfpuberty[~nanmask].copy()
+            # make sure the features are all float
+            mycleandf_puberty[all_features_puberty] = mycleandf_puberty[all_features_puberty].astype(float)
 
-        # make sure the features are all float
-        mycleandf_puberty[all_features_puberty] = mycleandf_puberty[all_features_puberty].astype(float)
+            # prediction features
+            Xpuberty = np.array(mycleandf_puberty[predictors[predictor]])
 
-        # prediction features
-        Xpuberty = np.array(mycleandf_puberty[predictors[predictor]])
+            # standardize the feature scales
+            scaler = StandardScaler().fit(Xpuberty)
+            Xpuberty = scaler.transform(Xpuberty)
 
-        # standardise the feature scales
-        scaler = StandardScaler().fit(Xpuberty)
-        Xpuberty = scaler.transform(Xpuberty)
-        # Xpuberty = pd.DataFrame(Xpuberty, columns=predictors)
+            # variable of interest (to be predicted)
+            ypuberty = np.array(mycleandf_puberty[[to_predict_puberty]])
 
-        # variable of interest (to be predicted)
-        ypuberty = np.array(mycleandf_puberty[[to_predict_puberty]])
+            group_split = np.array(mycleandf_puberty[[group_by]])[:, 0]
 
-        group_split = np.array(mycleandf_puberty[[group_by]])[:, 0]
+            # load fitted model
+            loaded_model = joblib.load(
+                open('/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/models/final_{}_abcd_{}_{}_model.pkl'.format(predictor, sex, subset), 'rb'))
 
-        # load fitted model
-        loaded_model = joblib.load(
-            open(
-                '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/models/final_{}_abcd_{}_model.pkl'.format(predictor, sex), 'rb'))
+            # Out of sample prediction using gam
+            kfold = GroupKFold(n_splits=10)
+            ypuberty_pred = ypuberty[:, 0] * 0
 
-        # Out of sample prediction using gam (new code)
-        # An inner loop CV = 10
-        kfold = GroupKFold(n_splits=10)
+            for i, (train_idx, test_idx) in enumerate(kfold.split(X=Xpuberty, y=ypuberty[:, 0], groups=group_split)):
+                print(f'Split #{i}')
 
-        # Calculate the puberty age
-        ypuberty_pred = ypuberty[:,0] * 0
+                gamreg = loaded_model.fit(Xpuberty[train_idx, :], ypuberty[train_idx, 0])
+                ypuberty_pred[test_idx] = gamreg.predict(Xpuberty[test_idx, :])
 
-        for i, (train_idx, test_idx) in enumerate(kfold.split(X=Xpuberty, y=ypuberty[:,0], groups=group_split)):
-            print(f'Split #{i}')
+            print('Model fitted...')
 
-            gamreg = loaded_model.fit(Xpuberty[train_idx,:], ypuberty[train_idx,0])
-            ypuberty_pred[test_idx] = gamreg.predict(Xpuberty[test_idx, :])
+            ypuberty_gap = ypuberty_pred - ypuberty[:, 0]
+            mycleandf_puberty['{}_{}_abcd'.format(predictor, subset)] = ypuberty_pred
+            mycleandf_puberty['{}_{}_gap_abcd'.format(predictor, subset)] = ypuberty_gap
 
-        print('Model fitted...')
-
-        ypuberty_gap = ypuberty_pred - ypuberty[:,0]
-        mycleandf_puberty['{}_abcd'.format(predictor)] = ypuberty_pred
-        mycleandf_puberty['{}_gap_abcd'.format(predictor)] = ypuberty_gap
-
-        mycleandf_puberty["{}_abcd_rtm".format(predictor)] = remove_regression_to_mean_effect(
-        mycleandf_puberty['age'],
-        mycleandf_puberty["{}_abcd".format(predictor)]
+            mycleandf_puberty["{}_{}_abcd_rtm".format(predictor, subset)] = remove_regression_to_mean_effect(
+                mycleandf_puberty['age'], mycleandf_puberty["{}_{}_abcd".format(predictor, subset)]
             )
 
-        mycleandf_puberty["{}_abcd_gap_rtm".format(predictor)] = \
-        mycleandf_puberty["{}_abcd_rtm".format(predictor)] - mycleandf_puberty["age"]
+            mycleandf_puberty["{}_{}_abcd_gap_rtm".format(predictor, subset)] = \
+                mycleandf_puberty["{}_{}_abcd_rtm".format(predictor, subset)] - mycleandf_puberty["age"]
 
+            mycleandf_puberty.to_csv(
+                r'/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/mycleandf_{}_gap_abcd_{}_{}.csv'.format(predictor, sex, subset),
+                index=True, header=True)
 
-
-        mycleandf_puberty.to_csv(
-            r'/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/mycleandf_{}_gap_abcd_{}.csv'.format(predictor, sex), index=True,
-            header=True)
-
-        mycleandf_puberty = mycleandf_puberty.set_index("id-wave")
-
-
-        # remove nan
-        # print(mydfpuberty.columns)
-        nanmask = mycleandf_puberty[["pds", "age"]].isna().any(axis=1)
-        mycleandf_puberty = mycleandf_puberty[~nanmask].copy()
-
-        # make sure the features are all float
-        mycleandf_puberty[["pds", "age"]] = mycleandf_puberty[["pds", "age"]].astype(float)
-
-        x = mycleandf_puberty["age"]
-        y = mycleandf_puberty["pds"]
-        reg = LinearRegression().fit(np.array([x]).T, np.array([y]).T)
-        reg_a = reg.coef_[0]
-        reg_b = reg.intercept_
-        y_reg_x = (y - reg_b - reg_a * x)
-
-        mycleandf_puberty["pds_age_regressed"] = y_reg_x
-        mycleandf_puberty["pds_age_regress_gap_abcd"] = y_reg_x
-        mycleandf_puberty["pds_age_regress_abcd"] = y_reg_x
-        mycleandf_puberty["pds_age_regress_abcd_rtm"] = y_reg_x
-        mycleandf_puberty["pds_age_regress_abcd_gap_rtm"] = y_reg_x
-
-
-        # mycleandf_puberty = mycleandf_puberty.set_index("id-wave")
-
-        mycleandf_puberty.to_csv(
-            r'/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/mycleandf_pds_age_regressgap_without_{}.csv'.format(
-                sex),
-            index=True, header=True)
+            mycleandf_puberty = mycleandf_puberty.set_index("id-wave")
+    
+    
+            # remove nan
+            # print(mydfpuberty.columns)
+            nanmask = mycleandf_puberty[["pds", "age"]].isna().any(axis=1)
+            mycleandf_puberty = mycleandf_puberty[~nanmask].copy()
+    
+            # make sure the features are all float
+            mycleandf_puberty[["pds", "age"]] = mycleandf_puberty[["pds", "age"]].astype(float)
+    
+            x = mycleandf_puberty["age"]
+            y = mycleandf_puberty["pds"]
+            reg = LinearRegression().fit(np.array([x]).T, np.array([y]).T)
+            reg_a = reg.coef_[0]
+            reg_b = reg.intercept_
+            y_reg_x = (y - reg_b - reg_a * x)
+    
+            mycleandf_puberty["pds_age_regressed"] = y_reg_x
+            mycleandf_puberty["pds_age_regress_gap_abcd"] = y_reg_x
+            mycleandf_puberty["pds_age_regress_abcd"] = y_reg_x
+            mycleandf_puberty["pds_age_regress_abcd_rtm"] = y_reg_x
+            mycleandf_puberty["pds_age_regress_abcd_gap_rtm"] = y_reg_x
+    
+    
+            # mycleandf_puberty = mycleandf_puberty.set_index("id-wave")
+    
+            mycleandf_puberty.to_csv(
+                r'/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/mycleandf_pds_age_regressgap_without_{}_{}.csv'.format(
+                    sex, subset),
+                index=True, header=True)
 
 
 
--- Puberty-age/run.py
+++ Puberty-age/run.py
@@ -11,6 +11,7 @@ outputdir = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/output'
 hormones = {"female": ["tst", "dhea"],
           "male": ["tst","dhea"]}
 
+subsets = {"1", "2", "3", "4"}
 
 pds = {
     "female": ['growth_spurt', "body_hair", 'skin_change', 'breast_develop', 'menarche'],
@@ -18,37 +19,38 @@ pds = {
 }
 
 for sex in sexes:
-    filename = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/typical_{}.csv'.format(sex)
-    dataname = 'typical_{}'.format(sex)
-    predictors = {
-        "hormone_age" : hormones[sex],
-        "pds_age": pds[sex],
-        "pubertyage":hormones[sex] + pds[sex],
+    for subset in subsets:
+        filename = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/typical_{}_{}.csv'.format(sex, subset)
+        dataname = 'typical_{}_{}'.format(sex, subset)
+        predictors = {
+            "hormone_age" : hormones[sex],
+            "pds_age": pds[sex],
+            "pubertyage":hormones[sex] + pds[sex],
 
-    }
-    for predictor in predictors:
-        print('#'*80)
-        print('# Running: sex=[{}], predictor=[{}]'.format(sex, predictor))
-        print('#'*80)
-        additional_dataset_test = None
+        }
+        for predictor in predictors:
+            print('#'*80)
+            print('# Running: sex=[{}], subset=[{}], predictor=[{}]'.format(sex, subset, predictor))
+            print('#'*80)
+            additional_dataset_test = None
 
-        to_predict = 'age'
-        group_by = 'family_id'
-        subject_col = 'id'
+            to_predict = 'age'
+            group_by = 'family_id'
+            subject_col = 'id'
 
-        # group split options
-        n_splits = 1
-        train_size = .9
-        random_state = 0
+            # group split options
+            n_splits = 1
+            train_size = .9
+            random_state = 0
 
-        #lambdas for gam grid search cross validation
-        lams = np.logspace(-5, 5, 50)
+            #lambdas for gam grid search cross validation
+            lams = np.logspace(-5, 5, 50)
 
-        model = prediction_git.run_predictions(
-            filename, f'{dataname}_group_split', predictors[predictor], to_predict, group_by=group_by,
-            n_splits=n_splits, train_size=train_size, random_state=random_state,
-            split_method='group', runmethod="svrrbf", additional_dataset_test=additional_dataset_test
-        )
+            model = prediction_git.run_predictions(
+                filename, f'{dataname}_group_split', predictors[predictor], to_predict, group_by=group_by,
+                n_splits=n_splits, train_size=train_size, random_state=random_state,
+                split_method='group', runmethod="gam", additional_dataset_test=additional_dataset_test
+            )
 
-        outputname = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/models/final_{}_abcd_{}_model.sav'.format(predictor, sex)
-        joblib.dump(model, open(outputname, 'wb'))
+            outputname = '/u/project/silvers/data/ABCD/cfm_flux_2023/puberty_age_gap/data/models/final_{}_abcd_{}_{}_model.sav'.format(predictor, sex, subset)
+            joblib.dump(model, open(outputname, 'wb'))
